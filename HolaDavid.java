13523ac1680fbcea5dd76c712b8ac4f99404110a
David Bodon <dbodonf01@educarex.es>
2023-06-12 22:07:10 +0000

    segundo

--- HolaDavid.java
+++ HolaDavid.java
@@ -1,7 +1,7 @@
 public class HolaDavid {
 
 	public static void main(String[] args) {		
-		System.out.println("Hola mi nombre es David");
+		System.out.println("Hola mi nombre es David, tengo 20 años");
 	}
 
 }
